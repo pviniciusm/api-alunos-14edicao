045ab95ef079c760030640b7914d1b248ee2562e
Paulo <pvinics@gmail.com>
2023-09-25 22:18:15 -0300

    add rota para listar usuarios

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,31 @@
 import { PrismaClient } from "@prisma/client";
+import express, { Request, Response } from "express";
 
 const repository = new PrismaClient();
+
+const app = express();
+app.use(express.json());
+
+app.get("/usuario", async (req: Request, res: Response) => {
+    // Lista os usuarios no BD
+    const result = await repository.usuario.findMany({
+        select: {
+            id: true,
+            nomeCompleto: true,
+            dtNascimento: true,
+            endereco: true,
+            email: true,
+            dthrCriacao: true,
+        },
+    });
+
+    res.status(200).send({
+        ok: true,
+        message: "Usuarios listados com sucesso",
+        data: result,
+    });
+});
+
+app.listen(3333, () => {
+    console.log("API está rodando na porta 3333");
+});
